a1fb566e650edf6c226e04762745377ef150bc6d
zirillifanplayr <nicola.zirilli@fanplayr.com>
2024-10-08 12:48:05 +0200

    fix Confirm vue

--- client/src/components/Confirm.vue
+++ client/src/components/Confirm.vue
@@ -14,9 +14,7 @@ const dialog = defineModel({ default: false })
             <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn text="Annulla" variant="plain" @click="dialog = false"></v-btn>
-                <template v-slot:action>
-                    <slot></slot>
-                </template>
+                <slot name="action"></slot>
             </v-card-actions>
         </v-card>
     </v-dialog>
