e7b14999e5bdafe216b88582e9dee3f6892cfaf8
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2024-10-22 18:28:47 +0200

    theme

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Axios from '@/services/client'
-import { onMounted, ref, onBeforeMount } from 'vue'
+import { ref, onBeforeMount } from 'vue'
 import router from '@/router'
 import { UserStore, SnackbarStore, ProgressStore, type IUser } from '@/stores'
 
@@ -10,6 +10,7 @@ const userStore = UserStore()
 const snackbarStore = SnackbarStore()
 const progressStore = ProgressStore()
 const user = ref({})
+const theme = ref('light')
 
 function login() {
   user.value = userStore.user
@@ -24,6 +25,10 @@ function reloadPage() {
   window.location.reload()
 }
 
+function toggleTheme() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+
 onBeforeMount(async () => {
   await userStore.checkAuthentication()
   user.value = userStore.user
@@ -33,21 +38,51 @@ onBeforeMount(async () => {
 
 <template>
   <v-responsive class="" max-height="100%">
-    <v-app>
+    <v-app :theme="theme">
       <v-app-bar>
         <template v-slot:prepend>
           <!-- <v-img scr="@/assets/chicomanda.png" alt="Chi Comanda"></v-img> -->
           <RouterLink to="/">
-            <img alt="Chi Comanda" class="logo" src="@/assets/chicomanda.png" style="margin-left: 8px; margin-top: 7px;" width="40"
-              height="40" />
+            <img alt="Chi Comanda" class="logo" src="@/assets/chicomanda.png" style="margin-left: 8px; margin-top: 7px;"
+              width="40" height="40" />
           </RouterLink>
         </template>
         <v-app-bar-title>
           <RouterLink to="/">CHI COMANDA</RouterLink>
         </v-app-bar-title>
-        <v-btn @click="axios.Logout()" v-if="userStore.isLoggedIn">
+        <!-- <v-btn @click="axios.Logout()" v-if="userStore.isLoggedIn">
           LOG OUT
-        </v-btn>
+        </v-btn> -->
+        <v-menu v-if="userStore.isLoggedIn">
+          <template v-slot:activator="{ props }">
+            <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item>
+              <v-list-item-title>
+                <v-btn @click="toggleTheme" v-if="userStore.isLoggedIn" variant="text">
+                  INVERTI TEMA
+                  <template v-slot:prepend>
+                    <v-icon>{{ theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
+                  </template>
+                </v-btn>
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>
+                <v-btn @click="axios.Logout()" v-if="userStore.isLoggedIn" variant="text">
+                  LOG OUT
+                  <template v-slot:prepend>
+                    <v-icon>mdi-logout</v-icon>
+                  </template>
+                </v-btn>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn v-else :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" text="Inverti tema"
+          slim @click="toggleTheme"></v-btn>
       </v-app-bar>
       <v-main>
         <RouterView v-model="user" @login="login" @reload="reload" />
