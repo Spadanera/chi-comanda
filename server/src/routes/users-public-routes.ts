ba5c8a85722a7d80892922dd9edeb63d9abb9fbd
Nicola Zirilli <nicola.zirilli@fanplayr.com>
2024-12-10 10:55:13 +0100

    feat: cache avatar

--- server/src/routes/users-public-routes.ts
+++ server/src/routes/users-public-routes.ts
@@ -6,6 +6,7 @@ const userPublicRouter: Router = router()
 userPublicRouter.get("/avatar/:id", async (req: Request, res: Response) => {
     try {
         const result = await userApi.getAvatar(+req.params.id)
+        res.set('Cache-Control', 'public, max-age=86400')
         res.status(200).json(result)
     } catch (error) {
         console.error(error)
